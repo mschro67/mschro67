4b3071a587a5589a92ec63b48d17a8632842e926
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-06 19:43:47 +0100

    Implement error handling in blink.py
    Added error handling for module imports and execution.

--- blink.py
+++ blink.py
@@ -1,11 +1,16 @@
 #by mschro67
 #for Raspberry Pi Pico
 
-from machine import Pin
-from time import sleep
+try:
+  from machine import Pin
+  from time import sleep
 
-led=Pin(25,Pin.OUT)
+  led=Pin(25,Pin.OUT)
 
-while True:
-  led.toggle
-  sleep(0.2)
+  while True:
+    led.toggle
+    sleep(0.2)
+except ModuleNotFoundError:
+  print("Module(s) not found")
+except Exception as e:
+  print(f"Error: {e}")
