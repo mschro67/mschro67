3830a8d614dda33f69be02b1d9b505c50529bc27
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-12 17:36:35 +0100

    Add ordner for change and upload toggle

---
+++ change/change.py
@@ -0,0 +1,15 @@
+#by mschro67
+#v2
+
+from machine import Pin
+
+with open("change/number.txt","r") as d:
+    a=int(d.read())
+    Pin(a,Pin.OUT).value(0)
+    a+=1
+    if a>4:
+        a=0
+    else:
+        Pin(a,Pin.OUT).toggle()
+    with open("change/number.txt","w") as b:
+        b.write(str(a))
