f44142f6bc37370edf164db63947e0bded9558e1
mschro67 <maximilian.s107@evt.schule.koeln>
2026-01-26 21:39:48 +0100

    Create button.py

---
+++ button.py
@@ -0,0 +1,16 @@
+#by mschro67
+#for the Orpheus Pico
+
+#pin 23: button
+#pin 25: led
+
+from machine import Pin
+from time import sleep
+
+button=Pin(23,Pin.IN,Pin.PULL_UP)
+led=Pin(25,Pin.OUT)
+
+while True:
+    if button.value() == 0: #0 = pressed
+        led.toggle()
+        sleep(0.3)
